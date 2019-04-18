fc76a8043e31f1d32eb6cdd592a66e98597eee49
Newton, Todd A <todd.newton@swri.org>
2019-04-18 12:22:50 -0500

    Console print outs cleaned up.  Print outs assigned a specific debug level to print, otherwise don't print. Comments added where appropriate. PEP8 conventions adhered to. README file created.  It includes a QUICKSTART command line argument list that utilizes the example MDL file and JSON schedule file, both of which are included in the MDL_Shell_Generator directory of the repo with the python utility.

--- Scenarios/FlightTesting/Utilities/TxOpScheduleViewer/MDL_Shell_Generator/mdl_shell_gen.py
+++ Scenarios/FlightTesting/Utilities/TxOpScheduleViewer/MDL_Shell_Generator/mdl_shell_gen.py
@@ -2,11 +2,20 @@
 # ------------------------------------------------------------------------------
 # ---                          MDL Shell Generator                           ---
 # ---                                                                        ---
-# --- Last Updated: April 17, 2019                                            ---
+# --- Last Updated: April 18, 2019                                           ---
+# ------------------------------------------------------------------------------
+# --- This utility trims a "master" MDL file into a smaller MDL file by      ---
+# --- trimming out RadioLinks, RadioGroups, and Radio NetworkNodes that are  ---
+# --- not needed.  The need is determined by a JSON file that contains a     ---
+# --- calendar-type resource file that specifies a time and the active set   ---
+# --- of RF links active at that time.  The resulting MDL file retains the   ---
+# --- RadioLinks, RadioGroups, and Radio NetworkNodes (and all associated    ---
+# --- references).  The resulting MDL file can serve as a snapshot of the    ---
+# --- active picture of the RF network components (e.g. Radios and Link      ---
+# --- Managers) for the specified time of day.                               ---
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-import sys
 import os
 import argparse
 from lxml import etree
@@ -47,44 +56,6 @@ class RadioLink:
         self.tx_sched.append(txop)
         self.tx_dur_per_epoch_usec += txop.duration_usec
 
-    def calc_max_latency(self, epoch_usec):
-        # initialize max_latency_usec with wrap-around TxOps
-        if len(self.tx_sched) > 0:
-            self.max_latency_usec = (int(epoch_usec) - (int(self.tx_sched[-1].stop_usec) + 1)) + \
-                                    int(self.tx_sched[0].start_usec)
-        else:
-            self.max_latency_usec = 0
-        
-        # iterate through the Link's TxOp Schedule, and compare latencies between TxOps with the previous max latency
-        for i in range(len(self.tx_sched) - 1):
-            temp_latency = int(self.tx_sched[i+1].start_usec) - (int(self.tx_sched[i].stop_usec) + 1)
-            if temp_latency > self.max_latency_usec:
-                self.max_latency_usec = temp_latency
-
-    def calc_alloc_bw_mbps(self, epoch_ms):
-        self.alloc_bw_mbps = ((int(self.tx_dur_per_epoch_usec) * (1000 / int(epoch_ms))) / 1000000) * MAX_BW_MBPS
-
-    def calc_latency_value(self, max_points_thd_ms, min_points_thd_ms):
-        if self.max_latency_usec < int(max_points_thd_ms*1000):
-            self.latency_point_value = 100
-        elif self.max_latency_usec < int(min_points_thd_ms*1000):
-            ans = 100 - (self.max_latency_usec - int(max_points_thd_ms*1000)) ** 2
-            if ans > 0:
-                self.latency_point_value = ans
-            else:
-                self.latency_point_value = 0
-        else:
-            self.latency_point_value = 0
-
-    def calc_throughput_value(self, min_points_thd, max_points_thd, coef):
-        alloc_bw_kbps = self.alloc_bw_mbps * 1000
-        if alloc_bw_kbps < min_points_thd:
-            self.throughput_point_value = 0.0
-        elif alloc_bw_kbps < max_points_thd:
-            self.throughput_point_value = 100 - (100 * (math.e ** ((-1 * coef) * alloc_bw_kbps)))
-        else:
-            self.throughput_point_value = 100 - (100 * (math.e ** ((-1 * coef) * max_points_thd)))
-
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
@@ -93,12 +64,13 @@ class RadioLink:
 if __name__ == "__main__":
     # Argument Parser Declarations
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', action='store', default=None, dest='MDL_MASTER', help='Master MDL File', type=str)
+    parser.add_argument('-m', action='store', default=None, dest='MDL_MASTER', help='Master MDL File', type=str,
+                        required=True)
     parser.add_argument('-o', action='store', default=None, dest='MDL_OUTPUT', help='Resulting MDL File', type=str)
     parser.add_argument('-s', action='store', default=None, dest='SCHEDULE',
-                        help='JSON file that contains the daily Flight Test Schedule', type=str)
+                        help='JSON file that contains the daily Flight Test Schedule', type=str, required=True)
     parser.add_argument('-t', action='store', default=None, dest='TIME',
-                        help='Time (hhmm, where "hh" is in range 0-23) for schedule lookup.', type=str)
+                        help='Time (hhmm, where "hh" is in range 0-23) for schedule lookup.', type=str, required=True)
     parser.add_argument('-d', action='count', default=0, dest='DEBUG', help='Set the Debug/Verbosity level')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1')
     cli_args = parser.parse_args()
@@ -110,8 +82,30 @@ if __name__ == "__main__":
     time = cli_args.TIME
     debug = cli_args.DEBUG
 
+    sched_links = []
     active_links = []
     l_radio_links = []
+
+    # Check to make sure the Master MDL file and the JSON schedule file both exist and are readable
+    if mdl_file is None:
+        print("  Must specify the Master MDL File ('-m').")
+        exit(-1)
+
+    if os.access(mdl_file, os.R_OK) is False:
+        print("  {} was not found.  File does not exist.".format(mdl_file))
+        print("  Must have a valid MDL file to continue.  Good-bye.")
+        exit(-2)
+
+    if sched_file is None:
+        print("  Must specify the JSON schedule file ('-s').")
+        exit(-3)
+
+    if os.access(sched_file, os.R_OK) is False:
+        print("  {} was not found.  File does not exist.".format(sched_file))
+        print("  Must have a valid schedule file to continue.  Good-bye.")
+        exit(-4)
+
+
 
     # Load the JSON file containing the schedule and the active links associated with each time in the schedule
     with open(sched_file) as f:
@@ -121,8 +115,8 @@ if __name__ == "__main__":
         if t['time'] == int(time):
             sched_links = t['links']
             if debug >= 1:
-                print("There are {0} Links Active at {1}:".format(len(sched_links), time))
-                for idx, link in enumerate(sched_links, start = 1):
+                print("  There are {0} Links Active at time {1:04}:".format(len(sched_links), int(time)))
+                for idx, link in enumerate(sched_links, start=1):
                     print("    Link {0}: {1}".format(idx, link))
 
     # Parse MDL file, and create the RAN Config (assuming only a single RAN Config)
@@ -134,11 +128,13 @@ if __name__ == "__main__":
     root_config_ver = root.find("mdl:ConfigurationVersion", namespaces=ns)
     root_config_ver.text = root_config_ver.text + '_' + time
 
-
     # Search MDL file for RadioLink elements that are not active, and remove them
-    print("Searching for Radio Links.  Removing any RadioLink element that is not active.")
+    print("  Searching for Radio Links.  Removing any RadioLink element that is not active.")
     radio_links_container = root.xpath("//mdl:RadioLinks", namespaces=ns)
     radio_links = root.xpath("//mdl:RadioLink", namespaces=ns)
+
+    if debug >= 1:
+        print("    Number of Radio Links in the Master MDL File: {}".format(len(radio_links)))
 
     for radio_link in radio_links:
         rlname = radio_link.find("mdl:Name", namespaces=ns).text
@@ -151,23 +147,31 @@ if __name__ == "__main__":
         rgs = root.xpath("//mdl:RadioGroup[@ID='{}']".format(rldst_idref["IDREF"]), namespaces=ns)
         rldst = rgs[0].find("mdl:GroupRFMACAddress", namespaces=ns).text
 
-        #print("Link {}: {} --> {}".format(rlname, rlsrc, rldst))
-        #print("parsed a link from MDL")
         link_is_not_active = True
         for sl in sched_links:
             if (sl['src'] == int(rlsrc)) and (sl['dst'] == int(rldst)):
                 new_link = RadioLink(rlname, rlid, rlsrc, rldst)
                 l_radio_links.append(new_link)
                 link_is_not_active = False
+                if debug >= 2:
+                    print("      |+| Link {} --> {}: ACTIVE.".format(rlsrc, rldst))
 
         if link_is_not_active:
-            #print("Link {} --> {} is not active.  Removing...".format(rlsrc, rldst))
             radio_links_container[0].remove(radio_link)
+            if debug >= 3:
+                print("      |-| Link {} --> {}: NOT ACTIVE.  Removing this RadioLink.".format(rlsrc, rldst))
+
+    if debug >= 1:
+        radio_links = root.xpath("//mdl:RadioLink", namespaces=ns)
+        print("    Number of Active Radios Links: {}".format(len(radio_links)))
 
     # Search MDL file for RadioGroup elements that are not active, and remove them
-    print("Searching for RadioGroups.  Removing any RadioGroup element that is not actived.")
+    print("  Searching for Radio Groups.  Removing any RadioGroup element that is not active.")
     radio_groups_container = root.xpath("//mdl:RadioGroups", namespaces=ns)
     radio_groups = root.xpath("//mdl:RadioGroup", namespaces=ns)
+
+    if debug >= 1:
+        print("    Number of Radio Groups in the Master MDL File: {}".format(len(radio_groups)))
 
     for radio_group in radio_groups:
         group_rf_mac_addr = int(radio_group.find("mdl:GroupRFMACAddress", namespaces=ns).text)
@@ -176,14 +180,20 @@ if __name__ == "__main__":
         for rl in l_radio_links:
             if int(rl.dst) == group_rf_mac_addr:
                 group_is_not_active = False
-                #print("found an RF Group")
+                if debug >= 2:
+                    print("      |+| Group RF MAC Address '{}': ACTIVE.".format(group_rf_mac_addr))
 
         if group_is_not_active:
             radio_groups_container[0].remove(radio_group)
+            if debug >= 3:
+                print("      |-| Group RF MAC Address '{}': NOT ACTIVE.".format(group_rf_mac_addr))
+
+    if debug >= 1:
+        radio_groups = root.xpath("//mdl:RadioGroup", namespaces=ns)
+        print("    Number of active Radio Groups : {}".format(len(radio_groups)))
 
     # Search MDL file for Radio NetworkNode elements that are not active, and remove them
-    print("Searching for Radios.  Removing any NetworkNode that contains an inactive Radio.")
-
+    print("  Searching for Radios.  Removing any NetworkNode that contains an inactive Radio.")
     radios = root.xpath("//mdl:TmNSRadio", namespaces=ns)
 
     if debug >= 1:
@@ -201,14 +211,14 @@ if __name__ == "__main__":
 
         radio_is_not_active = True
         for rl in l_radio_links:
-            #print("    rl.src --> rl.dst   |   src_mac --> dst_mac: {} --> {}   |   {} --> {}".format(rl.src, rl.dst, src_mac, dst_group_mac))
             if (int(rl.src) == src_mac) and (dst_group_mac is not None):
-                #print("    keep this Network Node.  This Radio is active.")
+                if debug >= 2:
+                    print("      |+| Radio {} is active.  Keeping this Radio's NetworkNode.".format(roleid))
                 radio_is_not_active = False
 
         if radio_is_not_active:
-            if debug >= 2:
-                print("    Radio '{}' is not active...Removing radio networknode...".format(roleid))
+            if debug >= 3:
+                print("      |-| Radio '{}' is not active.  Removing this Radio's NetworkNode.".format(roleid))
             tmnsapp = next(radio.iterancestors())
             tmnsapps = next(tmnsapp.iterancestors())
             networknode = next(tmnsapps.iterancestors())
@@ -217,28 +227,26 @@ if __name__ == "__main__":
 
     if debug >= 1:
         radios = root.xpath("//mdl:TmNSRadio", namespaces=ns)
-        print("    Number of Active Radios (Links): {}".format(len(radios)))
-
+        print("    Number of Active Radios (NetworkNodes): {}".format(len(radios)))
 
     # Search MDL file for QoSPolicy elements and remove all RadioLinkRef elements that are not active
-    print("Searching QoS Policies.  Removing any RadioLinkRef elements that reference inactive links.")
+    print("  Searching QoS Policies.  Removing any RadioLinkRef elements that reference inactive links.")
     qos_policies = root.xpath("//mdl:QoSPolicy", namespaces=ns)
-    for qp in qos_policies:
-        qos_rlrefs = qp.findall(".//mdl:RadioLinkRef", namespaces=ns)
+    for policy in qos_policies:
+        qos_rlrefs = policy.findall(".//mdl:RadioLinkRef", namespaces=ns)
         for rlref in qos_rlrefs:
-            if any(rl for rl in l_radio_links if rl.id==rlref.attrib["IDREF"]):
+            if any(rl for rl in l_radio_links if rl.id == rlref.attrib["IDREF"]):
                 if debug >= 2:
-                    print("    Found RF Link referenced: {}.".format(rlref.attrib["IDREF"]))
+                    print("      |+| Ref {} was found.".format(rlref.attrib["IDREF"]))
             else:
-                if debug >= 2:
-                    print("    Did not find RF Link referenced ({})...removing reference from QoS Policy.".
+                if debug >= 3:
+                    print("      |-| Ref {} was not found.  Removing reference from QoS Policy.".
                           format(rlref.attrib["IDREF"]))
                 rlref_container = next(rlref.iterancestors())
                 rlref_container.remove(rlref)
 
-
     # Search MDL file for Link Manager's App References, and remove the references to TMAs of radios that are not active
-    print("Searching Link Manager's app references.  Removing any TmNSAppRef elements that reference inactive radios.")
+    print("  Searching Link Manager's app references.  Removing TmNSAppRef elements that reference inactive radios.")
     link_managers = root.xpath("//mdl:TmNSLinkManager", namespaces=ns)
     tmns_apps = root.xpath("//mdl:TmNSApp", namespaces=ns)
     for lm in link_managers:
@@ -248,27 +256,34 @@ if __name__ == "__main__":
             for tma in tmns_apps:
                 if tma.attrib["ID"] == tmaref.attrib["IDREF"]:
                     ref_is_not_active = False
+                    if debug >= 2:
+                        roleid = tma.find("mdl:RoleID", namespaces=ns).text
+                        print("      |+| Reference to TMA with RoleID '{}' was found.".format(roleid))
 
             if ref_is_not_active:
+                if debug >= 3:
+                    print("      |-| Ref to TMA (ref: {}) was not found.  Removing reference from QoS Policy.".
+                          format(tmaref.attrib["IDREF"]))
                 tmaref_container = next(tmaref.iterancestors())
                 tmaref_container.remove(tmaref)
 
-
-    # Write trimmed MDL file to output file.
+    # Write trimmed MDL file to output file.  If output file was specified by the user on the command line, use that.
+    # Otherwise, set the output file to the name of the master MDL (input) file with the schedule time appended to it.
     output = etree.tostring(root, pretty_print=True)
 
     if mdl_output is None:
         temp, ext = os.path.splitext(mdl_file)
         mdl_output = temp + '_' + time + '.mdl'
-    if mdl_output is not None:
-        with open(mdl_output, 'wb') as f:
-            f.write(output)
 
-    if debug >= 5:
-        print("vvvvvvvvvvvvvvv ----- MDL OUTPUT FILE ----- vvvvvvvvvvvvvvv")
+    with open(mdl_output, 'wb') as f:
+        f.write(output)
+
+    # Print the trimmed MDL file contents to the terminal
+    if debug >= 4:
+        print("  vvvvvvvvvvvvvvv ----- MDL OUTPUT FILE ----- vvvvvvvvvvvvvvv")
         print(" ")
         print(output)
         print(" ")
-        print("^^^^^^^^^^^^^^^ ----- MDL OUTPUT FILE ----- ^^^^^^^^^^^^^^^")
+        print("  ^^^^^^^^^^^^^^^ ----- MDL OUTPUT FILE ----- ^^^^^^^^^^^^^^^")
 
-    print("Done.  The trimmed MDL File can be found at: {}".format(mdl_output))
+    print("  Done.  The trimmed MDL File can be found at: {}".format(mdl_output))
