4569e99a733dde587e026afaab21ac355cc13625
Newton, Todd A <todd.newton@swri.org>
2019-04-22 15:19:43 -0500

    Simplified the Data Rate Input Updater (renamed from Data Rate Input Generator).  Now, it just copies an existing file over at a specific time. Updated bw_allocs.json to include 19 radios, initialized to '0' allocated bandwidth. Added gen_datarate_scenario.py tool to generate the test directory and all associated data rate input files needed by the Data Rate Input Updater tool. Added a sample CSV file for crafting a scenario.  This file can be used as an input to the gen_datarate_scenario.py tool. Added README_DATARATE_GEN to describe how to use the gen_datarate_scenario.py and datarate_updater.py tools.

--- Scenarios/FlightTesting/Utilities/RadioQueue/datarate_updater.py
+++ Scenarios/FlightTesting/Utilities/RadioQueue/datarate_updater.py
@@ -1,128 +1,20 @@
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# ---      Data Rate Input Generator for Radio Queue Visualizer Utility      ---
+# ---       Data Rate Input Updater  for Radio Queue Visualizer Utility      ---
 # ---                                                                        ---
-# --- Last Updated: April 18, 2019                                        ---
+# --- Last Updated: April 19, 2019                                           ---
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-import sys
 import os
 import argparse
 import json
 from shutil import copyfile
-from collections import deque
-import threading
 import time
-import math
-import random
-
-next_timer = 0
-epoch_ms = 100                      # epoch size in milliseconds
-epoch_sec = epoch_ms / 1000         # epoch size converted to seconds
-epochs_per_sec = 1000 / epoch_ms    # number of epochs per second
-epoch_num = 0
 debug = 0                           # Debug value: initially 0, e.g. no debug
 
-radio_list = []                     # List of Radio objects
-
-msg_list = []
 
 # ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-
-
-class RadioDataInput:
-    """Class to contain the characteristics associated with the data input to a Radio."""
-
-    def __init__(self, name, rate_bps=0, burstiness=0.0, val_per_kb_tx=0.0):
-        self.name = name
-        self.input_rate_bps = rate_bps
-        self.burstiness = burstiness
-        self.value_per_kb_tx = val_per_kb_tx
-
-
-
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-
-
-def init_epoch_vals(ms):
-    global epoch_ms
-    global epoch_sec
-    global epochs_per_sec
-    
-    epoch_ms = ms                       # epoch size in milliseconds
-    epoch_sec = epoch_ms / 1000         # epoch size converted to seconds
-    epochs_per_sec = 1000 / epoch_ms    # number of epochs per second
-
-
-# ------------------------------------------------------------------------------
-
-
-def change_input_rate(rname, value):
-    global radio_list
-
-    for r in radio_list:
-        if r.name == rname:
-            r.input_rate_bps = value
-
-
-# ------------------------------------------------------------------------------
-
-
-def change_burstiness(rname, value):
-    global radio_list
-
-    for r in radio_list:
-        if r.name == rname:
-            r.burstiness = value
-
-
-# ------------------------------------------------------------------------------
-
-
-def change_value_per_tx(rname, value):
-    global radio_list
-
-    for r in radio_list:
-        if r.name == rname:
-            r.value_per_kb_tx = value
-
-
-# ------------------------------------------------------------------------------
-
-
-def init_radio_list(input_file):
-    global radio_list
-
-    with open(input_file, 'r') as f:
-        ldict_radios = json.load(f)
-
-    for d in ldict_radios:
-        name = d['RadioName']
-        newRDI = RadioDataInput(name)
-        radio_list.append(newRDI)
-
-
-# ------------------------------------------------------------------------------
-
-
-def update_input_rates_file(file, radios):
-    rlist = []
-
-    for r in radios:
-        radio_d = {}
-        radio_d['RadioName'] = r.name
-        radio_d['DataInRate-bps'] = r.input_rate_bps
-        radio_d['Burstiness'] = r.burstiness
-        radio_d['ValuePerKbTx'] = r.value_per_kb_tx
-        rlist.append(radio_d)
-
-    with open(file, 'w') as f:
-        json.dump(rlist, f, indent=4)
-
-
 # ------------------------------------------------------------------------------
 
 
