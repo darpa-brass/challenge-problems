c3914cd0448c668b2368208f94566beab0c985df
Joseph Hite <joseph.e.hite@vanderbilt.edu>
2019-05-07 15:52:18 -0500

    Json report now generated as part validation. Changed Error handling to raise exception instead of just exit program.

--- Scenarios/FlightTesting/Utilities/MdlValidation/validate.py
+++ Scenarios/FlightTesting/Utilities/MdlValidation/validate.py
@@ -1,8 +1,27 @@
 from lxml import etree
 from xmldiff import main, formatting
+import os
 import sys
 import re
 import argparse
+import json
+
+
+def write_error(message, report_data):
+    report_data['Error']['Type'] = 'IOError'
+    report_data['Error']['Message'] = message
+    write_report(report_data)
+    print(message, file=sys.stderr)
+
+
+def write_report(report_data):
+    cwd = os.getcwd()
+    report_dir = 'validation_log'
+    report_file = 'validation_report.json'
+    report_dir_path = os.path.join(cwd, report_dir)
+    report_file_path = os.path.join(report_dir_path, report_file)
+    with open(report_file_path, 'w') as f:
+        json.dump(report_data, f)
 
 
 def validate(filename_xsd, filename_xml, filename_xml_ground_truth):
@@ -13,24 +32,38 @@ def validate(filename_xsd, filename_xml, filename_xml_ground_truth):
     :param filename_xml: The filename of the XML to be validated.
     :param filename_xml_ground_truth: The filename (or file-like object) to compare against.
     """
+    # Initialize Report
+    validation = {"Well Formed": None,
+                  "Schema Valid": None,
+                  "Ground Truth": {"Diff": None,
+                                   "Matched": False},
+                  "Error": {"Message": None,
+                            "Type": None},
+                  "File Names": {"Ground Truth MDL": filename_xml_ground_truth,
+                                 "Transformed MDL": filename_xml}
+                  }
+
     # parse xml
     try:
         xml_doc = etree.parse(filename_xml)
         print('XML well formed, syntax ok.')
-
+        validation["Well Formed"] = True
     # check for file IO error
     except IOError:
-        print(f'Invalid XML file: {filename_xml}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Invalid XML file: {filename_xml}'
+        write_error(msg, validation)
+        raise
 
     # check for XML syntax errors
     except etree.XMLSyntaxError as err:
-        print(f'XML syntax Error: {err.error_log}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'XML syntax Error: {err.error_log}'
+        write_error(msg, validation)
+        raise
 
     except Exception as err:
-        print(f'Unknown error parsing XML, exiting: {err}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Unknown error parsing XML, exiting: {err}'
+        write_error(msg, validation)
+        raise
 
     # validate against schema
     try:
@@ -38,19 +71,23 @@ def validate(filename_xsd, filename_xml, filename_xml_ground_truth):
         xmlschema = etree.XMLSchema(xmlschema_doc)
         xmlschema.assertValid(xml_doc)
         print('XML valid, schema validation ok.')
+        validation["Schema Valid"] = True
 
     # check for file IO error
     except IOError:
-        print(f'Invalid schema file: {filename_xsd}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Invalid schema file: {filename_xsd}'
+        write_error(msg, validation)
+        raise
 
     except etree.DocumentInvalid as err:
-        print(f'Schema validation error: {err.error_log}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Schema validation error: {err.error_log}'
+        write_error(msg, validation)
+        raise
 
     except Exception as err:
-        print(f'Unknown error validating XML against schema, exiting: {err}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Unknown error validating XML against schema, exiting: {err}'
+        write_error(msg, validation)
+        raise
 
     # diff ground truth
     try:
@@ -59,23 +96,30 @@ def validate(filename_xsd, filename_xml, filename_xml_ground_truth):
         diff = main.diff_trees(xml_doc, xml_ground_truth, formatter=formatting.XMLFormatter())
         if "diff:" not in diff:
             print("Ground truth matches provided XML.")
+            validation["Ground Truth"]["Matched"] = True
         else:
             print("Ground truth not matched. Diff follows:")
             print(diff)
+            validation["Ground Truth"]["Diff"] = diff
 
     # check for file IO error
     except IOError:
-        print(f'Invalid ground truth file: {filename_xml_ground_truth}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Invalid ground truth file: {filename_xml_ground_truth}'
+        write_error(msg, validation)
+        raise
 
     # check for XML syntax errors
     except etree.XMLSyntaxError as err:
-        print(f'XML syntax error in ground truth: {err.error_log}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'XML syntax error in ground truth: {err.error_log}'
+        write_error(msg, validation)
+        raise
 
     except Exception as err:
-        print(f'Unknown error diffing XML, exiting: {err}', file=sys.stderr)
-        sys.exit(1)
+        msg = f'Unknown error diffing XML, exiting: {err}'
+        write_error(msg, validation)
+        raise
+
+    write_report(validation)
 
 
 def parse_xml(filename: str) -> object:
@@ -108,4 +152,7 @@ if __name__ == "__main__":
     parser.add_argument('--xml-ground-truth', action='store', dest='filename_xml_ground_truth', help='The XML file serving as ground truth.')
     cli_args = parser.parse_args()
 
-    validate(cli_args.filename_xsd, cli_args.filename_xml, cli_args.filename_xml_ground_truth)
+    try:
+        validate(cli_args.filename_xsd, cli_args.filename_xml, cli_args.filename_xml_ground_truth)
+    except Exception as err:
+        sys.exit(sys.exc_info())
