bffb41240f93f618084a77f79dc3aa6189fe0a1e
Austin Whittington <austin.whittington@swri.org>
2019-10-31 15:33:05 -0500

    finished uw demo scripts

--- Scenarios/FlightTesting/Utilities/demo/uw.py
+++ Scenarios/FlightTesting/Utilities/demo/uw.py
@@ -4,5 +4,5 @@ import boto3
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket("dev-uw-logs")
-key = 'bamboo/scheduler-env-UW-UWSCHED-20-master-682205c4327b72a5fd6f1bd6cb248c689ef6495d-release-10/BambooEvaluationInput.zip'
+key = 'bamboo/scheduler-env-demo/BambooEvaluationInput.zip'
 bucket.download_file(key, os.path.basename(key))
