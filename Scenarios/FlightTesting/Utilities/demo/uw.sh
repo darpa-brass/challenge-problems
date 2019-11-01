28cc534a0a6114fa9463eeb9785e70b464c4e16f
Austin Whittington <austin.whittington@swri.org>
2019-11-01 15:25:40 -0500

    switched uw demo scenario

--- Scenarios/FlightTesting/Utilities/demo/uw.sh
+++ Scenarios/FlightTesting/Utilities/demo/uw.sh
@@ -5,6 +5,6 @@ mkdir -p uw
 unzip BambooEvaluationInput.zip -d uw
 rm BambooEvaluationInput.zip
 
-python3 ../TxOpScheduleViewer/TxOpSchedViewer.py ./uw/BambooEvaluationInput/ta_schedule_output_15.mdl -s ./uw/BambooEvaluationInput/score_15.json
+python3 ../TxOpScheduleViewer/TxOpSchedViewer.py ./uw/BambooEvaluationInput/ta_schedule_output_2.mdl -s ./uw/BambooEvaluationInput/score_2.json
 
 python3 ../TxOpScheduleViewer/TxOpSchedViewer.py ./uw/BambooEvaluationInput/BRASS_Scenario4_After_Adaptation.mdl
