14658fcf7fd9d4b322f5312a840feac67a07599c
Austin Whittington <austin.whittington@swri.org>
2019-11-04 14:59:13 -0600

    added bbn demo scripts

--- Scenarios/FlightTesting/Utilities/demo/cra_cp2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import boto3
-
-s3 = boto3.resource('s3')
-bucket = s3.Bucket("dev-cra-logs")
-key = 'bamboo/CP2/demo/output/cp2/forward_switched_02_adapted_data.txt'
-bucket.download_file(key, os.path.basename(key))
